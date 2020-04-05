9fe9df0934014821753e8987c79a8572c1d39b89
Jasim Khan <Jasim Khan@DESKTOP-EPH9RMT>
2020-04-04 20:47:43 -0400

    Add project files.

---
+++ comp206-slotmachineDemo/Script/game.ts
@@ -0,0 +1,60 @@
+﻿var canvas;
+var stage: createjs.Stage;
+
+//game Objects
+var helloText: createjs.Text;
+var button: createjs.Bitmap;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+    helloText.rotation += 5;
+
+    stage.update();
+}
+
+function buttonClicked() {
+    helloText.text = "Good Bye";
+    helloText.regX = helloText.getBounds().width * 0.5;
+    helloText.regY = helloText.getBounds().height * 0.5;
+}
+
+function buttonOut() {
+    button.alpha = 1.0;
+}
+
+function buttonOver() {
+    button.alpha = 0.5;
+}
+function main() {
+
+    //bitmap button
+    button = new createjs.Bitmap("assets/images/bluebutton.jpg");
+    stage.addChild(button);
+    button.x = stage.canvas.width * 0.5;
+    button.y = stage.canvas.height * 0.5;
+    button.regX = button.getBounds().width * 0.5;
+    button.regY = button.getBounds().height * 0.5;
+    button.addEventListener("click", buttonClicked);
+    button.addEventListener("mouseover", buttonOver);
+    button.addEventListener("mouseout", buttonOut);
+
+    // this is my label
+    helloText = new createjs.Text("Hello world", "40px Consolas", "#000000");
+    stage.addChild(helloText);
+
+    helloText.x = stage.canvas.width * 0.5;
+    helloText.y = stage.canvas.height * 0.5;
+    helloText.regX = helloText.getBounds().width * 0.5;
+    helloText.regY = helloText.getBounds().height * 0.5;
+
+ 
+}
---
+++ comp206-slotmachineDemo/scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../script/lib/easeljs.min.js" />
+/// <reference path="../script/lib/preloadjs.min.js" />
+/// <reference path="../script/lib/soundjs.min.js" />
+/// <reference path="../script/lib/tweenjs.min.js" />
+/// <reference path="ai.0.22.9-build00167.js" />
